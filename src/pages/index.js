08f0aacf194c672b25240db1eb9fed8bc7a69b36
Natalia <dnataliacr@gmail.com>
2021-09-01 12:51:30 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,13 @@
 import * as React from "react";
+import Layout from "./components/layout/Layout";
 import Hero from "./components/home/Hero";
-// markup
+
 const IndexPage = () => {
-  return <Hero />;
+  return (
+    <Layout>
+      <Hero />
+    </Layout>
+  );
 };
 
 export default IndexPage;
