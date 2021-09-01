d929f6ce39ce6aaf7ca5c632703b46c2a9cc57c6
Natalia <dnataliacr@gmail.com>
2021-09-01 12:31:08 -0300

    Update Hero.js

--- src/pages/components/home/Hero.js
+++ src/pages/components/home/Hero.js
@@ -1,11 +1,22 @@
-import React from 'react';
-
-
+import React from "react";
+import video from "../../assets/video.mp4";
+import "./hero.css";
 const Hero = () => {
-    return (
-        <div>holi</div>
-    )
-}
+  return (
+    <div className="video-body">
+      <div className="video-row">
+        <video
+          src={video}
+          src={video}
+          type="video/mp4"
+          autoPlay
+          loop
+          muted
+          playsInline
+        />{" "}
+      </div>
+    </div>
+  );
+};
 
-
-export default Hero
+export default Hero;
